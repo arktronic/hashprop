dea6c5dbb8798102f0d4cfa481b8660f38ac4755
Arktronic <arktronic@gmail.com>
2014-10-02 08:49:21 -0400

    Fixed installation by adding a custom action to register/unregister in COM

---
+++ HashProp/InstallerHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace SetupHelper
+{
+    [RunInstaller(true)]
+    public partial class InstallerHelper : System.Configuration.Install.Installer
+    {
+        public InstallerHelper()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAfterInstall(IDictionary savedState)
+        {
+            RegistrationServices regsrv = new RegistrationServices();
+            regsrv.RegisterAssembly(GetType().Assembly, AssemblyRegistrationFlags.SetCodeBase);
+            base.OnAfterInstall(savedState);
+        }
+
+        protected override void OnAfterUninstall(IDictionary savedState)
+        {
+            RegistrationServices regsrv = new RegistrationServices();
+            regsrv.UnregisterAssembly(GetType().Assembly);
+            base.OnAfterUninstall(savedState);
+        }
+    }
+}
